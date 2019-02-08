dc12658d76d1e10b8416e6ca13a1b301a7d3b5f9
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-08 23:21:16 +0100

    reader boolean a -> option a

--- src/main/scala/transformation/ReaderBooleanToOption.scala
+++ src/main/scala/transformation/ReaderBooleanToOption.scala
@@ -1,8 +1,12 @@
 package transformation
 
+import functor.Reader
+
 /**
   * Created by mtumilowicz on 2019-02-08.
   */
 object ReaderBooleanToOption {
-
+  def trivial[A](reader: Reader[Boolean, A]): Option[A] = None
+  def truth[A](reader: Reader[Boolean, A]): Option[A] = Some(reader(true))
+  def untruth[A](reader: Reader[Boolean, A]): Option[A] = Some(reader(false))
 }
--- src/test/scala/NaturalTransformationsTest.scala
+++ src/test/scala/NaturalTransformationsTest.scala
@@ -11,19 +11,19 @@ class NaturalTransformationsTest extends FunSuite with Matchers {
     ListToOption.safeHead(List(1)) should be(Some(1))
     ListToOption.safeHead(List()) should be(None)
   }
-  
+
   test("option -> list") {
-    OptionToList.toList(None) should be (List())
-    OptionToList.toList(Some(1)) should be (List(1))
+    OptionToList.toList(None) should be(List())
+    OptionToList.toList(Some(1)) should be(List(1))
   }
 
-  test("reader -> option: trivial, obvious") {
+  test("reader () a -> option a: trivial, obvious") {
     def reader: Reader[Unit, String] = _ => "a"
 
     ReaderUnitToOption.trivial(reader) should be(None)
     ReaderUnitToOption.obvious(reader) should be(Some("a"))
   }
-  test("reader -> list: trivial, obvious") {
+  test("reader () a -> list a: trivial, obvious") {
     def reader: Reader[Unit, String] = _ => "a"
 
     ReaderUnitToList.trivial(reader) should be(List())
@@ -33,5 +33,16 @@ class NaturalTransformationsTest extends FunSuite with Matchers {
   test("[a] -> const int a: length") {
     ListToInt.lengthOf(List()) should be(Const(0))
     ListToInt.lengthOf(List(1, 2, 3, 4)) should be(Const(4))
+  }
+
+  test("reader boolean a -> option a: trivial, truth, untruth") {
+    def reader: Reader[Boolean, String] = {
+      case true => "truth"
+      case false => "untruth"
+    }
+    
+    ReaderBooleanToOption.trivial(reader) should be (None)
+    ReaderBooleanToOption.truth(reader) should be (Some(true))
+    ReaderBooleanToOption.untruth(reader) should be (Some(false))
   }
 }
